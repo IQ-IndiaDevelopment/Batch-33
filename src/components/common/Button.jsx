b1480a8ef2ef13dcd96b423eb9127c3d5088c520
Koushal <koushalpatel555@gmail.com>
2023-05-07 12:33:07 +0530

    Paragraph added

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({name,styles,onClick}) => {
-  return <button className={`${styles} w-[120px]`} onClick={onClick}>{name}</button>;
+  return <button className={`${styles} w-[250px]`} onClick={onClick}>{name}</button>;
 };
 
 export default Button;
