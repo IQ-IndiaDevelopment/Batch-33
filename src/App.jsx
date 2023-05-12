0743768f1f15e403ddb0e782f9bcdb9ee927951d
Shubhankar <bhadra.shubhankar@gmail.com>
2023-05-12 17:49:41 +0530

    all modification

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <>
-      <nav><p><p>TODO LIST</p></nav>
+      <nav class="flex item-center border-b-2 border-black bg-gray-400 ">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Deloitte.svg/1280px-Deloitte.svg.png" alt-="Deloitte LoGo" className="h-10" />
+        
+  <pre>                                                                                                                <a className="hover:text-green-400 bg-white border-solid rounded-md border-2 " href="#">HOME</a>  <a className="hover:text-green-400 bg-white border-solid rounded-md" href="#">ABOUT</a>  <a className="hover:text-green-400 bg-white border-solid rounded-md" href="#">CONTACT-US</a>  <a className="hover:text-green-400 bg-white border-solid rounded-md" href="#">BLOG</a>  </pre>
+
+      
+      </nav>
       <TodoList />
     </>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -22,13 +22,21 @@ const TodoList = () => {
   console.log(taskList, "taskList");
 
   return (
-    <div>
-      <div>
-        <input type="text" value={taskName} onChange={handleChange} />
-        <Button name="ADD" styles="" onClick={handleClick} />
+    <>
+    <center>
+    <div  className="bg-gradient-to-r from-green-400 via-red-500 to-black relative item- center block max-w-sm px-20 border-grey-100 rounded-lg shadow-md dark:bg-grey-700 dark:border-grey-800 dark:hover:bg-grey-700">
+      <center> <br></br>
+      <div> 
+        <input type="text" placeholder="                  TO-DO-LIST " value={taskName} onChange={handleChange} className="border border-green-600 rounded-md h-15 w-[248px]	focus:border-green-600 focus: outline-0 focus:ring-0 w-[20%]" />
+        <Button name="ADD" styles="w-30 h-6 bg-green-600 rounded-lg" onClick={handleClick} />
       </div>
-      <List taskList={taskList}/>
+      <List taskList={taskList}/></center>
+    </div></center> <div>
+      <img src="https://wallpapercave.com/wp/wp11400962.jpg"></img>
     </div>
+    </>
+    
+    
   );
 };
 
