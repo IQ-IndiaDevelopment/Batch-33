4d100a1a5371aa0ff99d8a73812b183c321a6417
shrupin2511@gmail.com <shrupin2511@gmail.com>
2023-05-12 17:52:50 +0530

    added an image in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,25 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <>
-      <nav>TODO LIST</nav>
+    <body>
+      <style><background color="yellow"></background></style>
+    </body>
+    <nav>
+      <div className="container">
+
+        <pre>
+        <img src="https://images-platform.99static.com/gFh93jcX-dIsHdImoQBGVUsvR7g=/0x0:979x979/500x500/top/smart/99designs-contests-attachments/95/95767/attachment_95767955"height="100px" width="100px"></img>
+        
+        <a href="#">Images</a> <a href="#">Reviews</a> <a href="#">Blog</a> <a href="#">Contact</a>
+
+        </pre>
+        
+      </div>
+      
+     </nav> 
+     <h1 ><b>TODO LIST</b></h1>
+     
+      
       <TodoList />
     </>
   );
