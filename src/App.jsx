1d4149a0693f3e82cd54ef656be9ed5e64898e2b
Hemant <20hemantk04@gmail.com>
2023-05-13 10:41:00 +0530

    File Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <>
-      <nav>TODO LIST</nav>
-      <TodoList />
+      <nav className="border-solid border-2 border-indigo-600 bg-cyan-500 hover:bg-cyan-600 "><p>TODO LIST</p></nav>
+
+      <h1>TodoList </h1>
+      <h1>React js code</h1>
     </>
   );
 }
